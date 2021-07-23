43fe3d2bef39941a8b3b480e011005b6a91d6a47
Martin Reynolds <mtreynolds@me.com>
2021-07-23 13:05:06 +0000

    refactor(pocs/node/probot): resolve linting errors for standardjs

--- pocs/node/probot/index.js
+++ pocs/node/probot/index.js
@@ -4,18 +4,18 @@
  */
 module.exports = (app) => {
   // Your code here
-  app.log.info("Yay, the app was loaded!");
+  app.log.info('Yay, the app was loaded!')
 
-  app.on("issues.opened", async (context) => {
+  app.on('issues.opened', async (context) => {
     const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    return context.octokit.issues.createComment(issueComment);
-  });
+      body: 'Thanks for opening this issue!'
+    })
+    return context.octokit.issues.createComment(issueComment)
+  })
 
   // For more information on building apps:
   // https://probot.github.io/docs/
 
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
-};
+}
